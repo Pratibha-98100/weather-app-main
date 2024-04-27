d5453dab007fd8e3ab627f9d475e7af95297f50a
Pratibha <pratibha98100@gmail.com>
2024-04-27 16:13:22 +0530

    this is my project

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = 'cd16dbe206d8a862fa64743ae8ca4971';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
